e4f04c49c8ecd00dc76e5e6868aa909c915fc0c6
itggot-elias-stenhede <elias.stenhedejohansson@itggot.se>
2019-02-21 10:54:52 +0100

    did almost everything

--- blogg/app.rb
+++ blogg/app.rb
@@ -5,8 +5,7 @@ require 'bcrypt'
 enable :sessions
 
 get ("/") do
-    session.destroy
-    slim(:home)
+    redirect("/home")
 end
 
 get ("/login") do
@@ -14,7 +13,7 @@ get ("/login") do
 end
 
 post ("/login") do
-    db = SQLite3::Database.new("db/user_pass.db")
+    db = SQLite3::Database.new("db/blog.db")
 
     hashed_pass = db.execute("SELECT Password FROM users WHERE Username = '#{params["Username"]}'")
     
@@ -29,12 +28,8 @@ post ("/login") do
         session[:username] = params["Username"]
         redirect("/loggedin")
     else
-        redirect("/login")
+        redirect("/failed")
     end
-end
-
-get ("/home") do
-    slim(:home)
 end
 
 get ("/register") do
@@ -42,7 +37,7 @@ get ("/register") do
 end
 
 post ("/register") do
-    db = SQLite3::Database.new("db/user_pass.db")
+    db = SQLite3::Database.new("db/blog.db")
     hashed_pass = BCrypt::Password.create("#{params["Password"]}")
     db.execute("INSERT INTO users (Username, Password) VALUES (?, ?)", params["Username"], hashed_pass)
     redirect("/login")
@@ -51,5 +46,28 @@ end
 get ("/loggedin") do
     if session[:loggedin]
         slim(:loggedin)
+    else
+        redirect("/permission")
     end
+end
+
+get ("/failed") do
+    slim(:failed)
+end
+
+get("/permission") do
+    slim(:permission)
+end
+
+post("/makepost") do
+    db = SQLite3::Database.new("db/blog.db")
+    db.execute("INSERT INTO posts (Username, Header, Text, Time) VALUES (?, ?, ?, ?)", session[:username], params["header"], params["text"], Time.now.to_s[0..9])
+    redirect("/home")
+end
+
+get("/home") do
+    db = SQLite3::Database.new("db/blog.db")
+    session[:blogposts] = db.execute("SELECT * FROM posts")
+
+    slim(:home)
 end
